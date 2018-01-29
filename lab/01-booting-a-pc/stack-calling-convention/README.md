4255ff4ca7fafe27ea4d4ad95f0139c44a0a9d0a
itkq <kuogsi@gmail.com>
2018-01-29 17:42:39 +0900

    Understanding stack calling convention

---
+++ lab/01-booting-a-pc/stack-calling-convention/README.md
@@ -0,0 +1,3 @@
+ref:
+
+- http://d.hatena.ne.jp/yz2cm/20130526/1369591965
---
+++ lab/01-booting-a-pc/stack-calling-convention/f1.c
@@ -0,0 +1,11 @@
+int add(int a, int b)
+{
+	return a + b;
+}
+
+int main(int argc, char **argv)
+{
+	int c = add(1, 2);
+	return 0;
+}
+
