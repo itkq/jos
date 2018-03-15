63d2860dad55afa547a6da75ec5f055a06d208c9
itkq <kuogsi@gmail.com>
2018-03-15 20:15:34 +0900

    Fix libmain (Lab 3-8)

--- lib/libmain.c
+++ lib/libmain.c
@@ -12,7 +12,7 @@ void
 libmain(int argc, char **argv)
 {
 	// set thisenv to point at our Env structure in envs[].
-	thisenv = envs;
+	thisenv = &envs[ENVX(sys_getenvid())];
 
 	// save the name of the program so that panic() can use it
 	if (argc > 0)
